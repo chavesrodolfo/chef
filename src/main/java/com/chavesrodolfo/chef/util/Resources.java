c5be8765c643a966e281f915da686b554179b478
chavesrodolfo@gmail.com <chavesrodolfo@gmail.com>
2013-09-13 20:57:46 -0300

    commit tests

---
+++ src/main/java/com/chavesrodolfo/chef/util/Resources.java
@@ -0,0 +1,20 @@
+package com.chavesrodolfo.chef.util;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.logging.Logger;
+
+public class Resources {
+    @SuppressWarnings("unused")
+    @Produces
+    @PersistenceContext
+    private EntityManager em;
+
+    @Produces
+    public Logger produceLog(InjectionPoint injectionPoint) {
+        return Logger.getLogger(injectionPoint.getMember().getDeclaringClass().getName());
+    }
+
+}
